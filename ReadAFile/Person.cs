c992e1af114984995feace442739bcc59e94fed6
Coralgoods <91501799+Coralgoods@users.noreply.github.com>
2022-07-06 14:53:28 -0400

    Initial upload

---
+++ ReadAFile/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ReadAFile
+{
+    public class Person
+    {
+        //Constructor
+        public Person(string userName, string firstName, string URL)
+        {
+
+            this.UserID = userName;
+            this.FirstName = firstName;
+            this.URL = URL;
+
+        }
+
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string URL { get; set; }
+
+        public override string ToString()
+        {
+            return "UserID: " + UserID + "FirstName: " + FirstName + "URL: " + URL; 
+        }
+    }
+}
---
+++ ReadAFile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"/Users/eduardodiaz/Desktop/NameList.txt";
+
+            List<string> lines = new List<string>(); 
+            List<Person> people = new List<Person>();
+            lines = File.ReadAllLines(filePath).ToList();
+
+
+
+            //You don't really need it. 
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+                Person p = new Person(items[0], items[1], items[2]);
+                people.Add(p); 
+            }
+
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p); 
+            }
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
